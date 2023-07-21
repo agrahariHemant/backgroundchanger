a659abbd49a84eac8bd446f5dacbe0d5d6ef968b
Hemant Kumar Agrahari <139849908+agrahariHemant@users.noreply.github.com>
2023-07-21 05:22:02 +0000

    background color changer

---
+++ backgroundchanger/script.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id === 'orange'){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id === 'pink'){
+            body.style.backgroundColor=e.target.id
+        }
+    })
+})
